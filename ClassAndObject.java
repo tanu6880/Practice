9db95d945957e20af4c6e871d0b5cd377baaea56
Tanu <tanusaini6880@gmail.com>
2023-08-28 15:27:26 +0530

    Simple example of class and object

---
+++ ClassAndObject.java
@@ -0,0 +1,19 @@
+package org.example.practice;
+
+class Car {
+
+    void GetMessage()
+    {
+        System.out.println("i am message of getmessage method");
+    }
+
+}
+public class ClassAndObject
+{
+    public static void main(String []args)
+    {
+        Car car1 = new Car();
+        car1.GetMessage();
+    }
+
+}
