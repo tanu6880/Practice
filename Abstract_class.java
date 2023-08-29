755e11b93bdf1379b1ca29ae431a49dd5f144932
Tanu <tanusaini6880@gmail.com>
2023-08-29 15:40:10 +0530

    abstract class example

--- Abstract_class.java
+++ Abstract_class.java
@@ -1,2 +1,31 @@
-package org.example.practice;public class Abstract_class {
+package org.example.practice;
+
+abstract class Bikes
+{
+    abstract void run();
+    int price;
+    Bikes()
+    {
+        price = 1000000;
+    }
+}
+class honda extends Bikes
+{
+    void run()
+    {
+        System.out.println("price is : "+price);
+        System.out.println("Running Safely");
+    }
+}
+abstract public class Abstract_class {
+    public static void main(String[] args) {
+      //  Bikes B1 = new Bikes() ; error
+        // only obj can not create of abstract class otherwise can create constructor , member, method even main body
+        Bikes B1 = new honda();
+        honda obj = new honda();
+        obj.run();
+        B1.run();
+
+
+    }
 }
