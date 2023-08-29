95fecb0856b8381a26ae580eb432c017a9f45a08
Tanu <tanusaini6880@gmail.com>
2023-08-29 12:10:58 +0530

    parmeterized Constructor in inheritence

--- parameterized_cons_inheritence.java
+++ parameterized_cons_inheritence.java
@@ -1,2 +1,31 @@
-package org.example.practice;public class parameterized_cons_inheritence {
+package org.example.practice;
+
+class Base
+{
+    int x;
+    Base(int x)
+    {
+        this.x = x;
+    }
+}
+class derived extends Base
+{
+    int y;
+    derived(int x,int y)
+    {
+        super(x);
+        this.y = y;
+    }
+    void print()
+    {
+        System.out.println("X " + x +", Y "+y);
+    }
+
+}
+public class parameterized_cons_inheritence {
+    public static void main(String[] args) {
+        derived d1 = new derived(2,3);
+        d1.print();
+    }
+
 }
