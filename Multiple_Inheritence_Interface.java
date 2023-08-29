727c76873c89fa3ad083d225f6ce71f872e64ed3
Tanu <tanusaini6880@gmail.com>
2023-08-29 15:42:10 +0530

    Example multiple inheritence by interface

--- Multiple_Inheritence_Interface.java
+++ Multiple_Inheritence_Interface.java
@@ -1,2 +1,33 @@
-package org.example.practice;public class Multiple_Inheritence_Interface {
+package org.example.practice;
+
+interface father
+{
+    int gift=10;
+    void gift();
+
+}
+interface mother
+{
+    void food();
+    String food = "pizzza";
+}
+class son implements father,mother
+{
+    public void gift()
+    {
+        System.out.println("i found "+gift+" gift from father");
+    }
+    public void food()
+    {
+        System.out.println("i have had "+food+" from mother");
+    }
+}
+public class Multiple_Inheritence_Interface {
+    public static void main(String[] args) {
+
+        son s1 = new son();
+        s1.food();
+        s1.gift();
+
+    }
 }
